cf4629079326a8b4399bea8e780b932ee2802698
kanugoyal <kanugoyal716@gmail.com>
2022-08-01 08:44:44 +0530

    onclick function in button

--- pdf.js
+++ pdf.js
@@ -1,5 +1,7 @@
-$(document).ready(function(){
-    $('#download').click(function(){
-        $('#resume').printThis();
-    });
-});
+function generatePDF(){
+    const element = document.getElementById("resume");
+
+    html2pdf()
+    .from(element)
+    .save();
+}
